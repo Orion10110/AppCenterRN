88bf58865b37f95f1a37045558e98f56c15fd8d5
Artur Kalach <artur_kalach@epam.com>
2020-08-25 17:03:03 +0300

    CodePush

--- App.js
+++ App.js
@@ -5,7 +5,6 @@
  * @format
  * @flow strict-local
  */
-import Push from 'appcenter-push';
 
 import React from 'react';
 import {
@@ -15,10 +14,7 @@ import {
   View,
   Text,
   StatusBar,
-  AppState,
-  Alert,
 } from 'react-native';
-
 import {
   Header,
   LearnMoreLinks,
@@ -26,8 +22,14 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import codePush from 'react-native-code-push';
 
-const App: () => React$Node = () => {
+let codePushOptions = {
+  updateDialog: true,
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+};
+
+const App = codePush(codePushOptions)(() => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -85,39 +87,6 @@ const App: () => React$Node = () => {
       </SafeAreaView>
     </>
   );
-};
-
-Push.setListener({
-  onPushNotificationReceived: function (pushNotification) {
-    let message = pushNotification.message;
-    let title = pushNotification.title;
-
-    if (message === null) {
-      // Android messages received in the background don't include a message. On Android, that fact can be used to
-      // check if the message was received in the background or foreground. For iOS the message is always present.
-      title = 'Android background';
-      message = '<empty>';
-    }
-
-    // Custom name/value pairs set in the App Center web portal are in customProperties
-    if (
-      pushNotification.customProperties &&
-      Object.keys(pushNotification.customProperties).length > 0
-    ) {
-      message +=
-        '\nCustom properties:\n' +
-        JSON.stringify(pushNotification.customProperties);
-    }
-
-    if (AppState.currentState === 'active') {
-      Alert.alert(title, message);
-    } else {
-      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
-      // In this case you'll want to save off the notification info and wait until the app is fully shown
-      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
-      // when the app is fully in the foreground.
-    }
-  },
 });
 
 const styles = StyleSheet.create({
